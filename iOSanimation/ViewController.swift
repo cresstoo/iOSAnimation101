0d3cb472696e423072fb3058bab61e20a993fc9d
cresstoo <cress@duran.im>
2015-07-20 19:54:32 +0800

    Initial Commit

---
+++ iOSanimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSanimation
+//
+//  Created by 哈吉马鹿咚 on 15/7/20.
+//  Copyright (c) 2015年 imduran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
